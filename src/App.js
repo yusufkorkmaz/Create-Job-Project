9582536cdb4d989352aa6d8689ecf5ec58adfef5
yusufkorkmaz <yusufkorkmazz98@hotmail.com>
2022-12-26 18:16:02 +0300

    create new job section added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import { Button } from '@mui/material';
-import React from 'react';
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { Box } from '@mui/system';
+
+import React, { useState } from 'react';
 import './App.scss';
 
 function App() {
+
+  const [priority, setPriority] = useState('');
+
+  const handleChange = (event) => {
+    setPriority(event.target.value);
+  };
+
   return (
     <div className="App">
-             <Button variant="contained">Hello World</Button>
+      <Box className="create-new-job-wrapper">
+
+        <h2>Create New Job</h2>
+
+        <Box className="create-job-items">
+          <TextField required fullWidth id="outlined-basic" label="Job Name" variant="outlined" />
+
+          <Box className="select-priority-box">
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Priority *</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                label="Priority *"
+                id="demo-simple-select"
+                fullWidth
+                value={priority}
+                onChange={handleChange}
+              >
+                <MenuItem value={'Urgent'}>Urgent</MenuItem>
+                <MenuItem value={'Regular'}>Regular</MenuItem>
+                <MenuItem value={'Trivial'}>Trivial</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+
+          <Button className='create-button' variant="contained">Create</Button>
+        </Box>
+      </Box>
     </div>
   );
 }
